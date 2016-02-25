99b0ecff937565823a341abec29e3ce529f305b5
gfocas <grfocas@gmail.com>
2016-02-25 18:19:03 -0300

    Primeiro commit - Comprovando a parada do IsBackground=true

---
+++ Estudos_70_483/ConsoleApplication/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void ThreadMethod()
+        {
+            Console.WriteLine("Estou na outra thread");
+            Thread.Sleep(5000);
+        }
+
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            //t.IsBackground = false;
+            t.Start();
+        }
+    }
+}
